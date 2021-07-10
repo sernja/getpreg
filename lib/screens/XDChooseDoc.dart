ea0204df0748d80ce7df35e489b41eb76cc2f7ad
sern <sanghit@hotmail.com>
2021-07-10 19:09:19 +0700

    fix bug test choosedoc, add automate

--- lib/screens/XDChooseDoc.dart
+++ lib/screens/XDChooseDoc.dart
@@ -9,13 +9,16 @@ import './XDDocFilter1.dart';
 class XDChooseDoc extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-          height: 200,
-          width: double.infinity,
-          color: Colors.red,
-          margin: EdgeInsets.all(20),
-        ),
-      );
+      body: Container(
+        margin: EdgeInsets.only(left: 0, right: 0, top: 0, bottom: 0),
+        width: 280,
+        height: 280,
+        child: Image.network(
+              'https://images.unsplash.com/photo-1547721064-da6cfb341d50',
+              width: 280.0,
+            ),
+      ),
+    );
     //  return MaterialApp(
     //   debugShowCheckedModeBanner: false,
     //   home: Scaffold(
