af4eee3d0904608a9786e559ebe67553348832cf
sern <sanghit@hotmail.com>
2021-08-05 12:21:31 +0700

    finish Splesh screen UI

--- lib/screen/Splesh.dart
+++ lib/screen/Splesh.dart
@@ -1,54 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
-class Splesh extends StatelessWidget {
+class Splesh extends StatefulWidget {
   @override
+  _Splesh createState() => new _Splesh();
+}
+
+class _Splesh extends State<Splesh> {
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       body: Container(
-        padding: const EdgeInsets.all(15),
+        padding: const EdgeInsets.all(10),
         width: double.infinity,
         height: double.infinity,
         decoration: BoxDecoration(
           image: DecorationImage(
-            image: const AssetImage('assets/images/bg.jpg'),
+            image: AssetImage("assets/images/bg.jpg"),
             fit: BoxFit.fill,
             colorFilter: new ColorFilter.mode(
                 Colors.black.withOpacity(0.65), BlendMode.dstIn),
           ),
         ),
-        child: SafeArea(
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  // width: 120,
-                  // height: MediaQuery.of(context).size.width / 0.9,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: AssetImage("assets/images/bg_withe.png"),
-                      fit: BoxFit.fill,
-                    ),
-                  ),
-                  padding: const EdgeInsets.all(15),
-                  child: Column(
-                    children: [
-                      Text(
-                        'กำลังมองหาคุณหมอใกล้คุณ?',
-                        style: TextStyle(
-                          fontFamily: 'FC Minimal',
-                          fontSize: 25,
-                          color: const Color(0xff111111),
-                          fontWeight: FontWeight.w700,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+        child: CupertinoAlertDialog(
+          title: Text("กำลังมองหาคุณหมอใกล้คุณ?"),
+          actions: <Widget>[
+            CupertinoDialogAction(
+              child: Text("ใช่"),
+              onPressed: () {
+                Fluttertoast.showToast(
+                    msg: "ใช่", toastLength: Toast.LENGTH_SHORT);
+                Navigator.of(context).pop();
+              },
             ),
-          ),
+            CupertinoDialogAction(
+              child: Text("ไม่ใช่"),
+              onPressed: () {
+                Fluttertoast.showToast(
+                    msg: "ไม่ใช่", toastLength: Toast.LENGTH_SHORT);
+                Navigator.of(context).pop();
+              },
+            )
+          ],
         ),
       ),
     );
