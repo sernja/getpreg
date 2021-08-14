cedb73ff9ec5fbfbdcc6e0ebfbbd24ba4e57b2b4
sern <sanghit@hotmail.com>
2021-08-14 12:16:12 +0700

    change floating_search_bar to textformfield

--- lib/screen/Tours.dart
+++ lib/screen/Tours.dart
@@ -1,82 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
 
 class Tours extends StatefulWidget {
   @override
   _Tours createState() => new _Tours();
 }
 
-// class _Tours extends State<Tours> {
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       // This is handled by the search bar itself.
-//       resizeToAvoidBottomInset: false,
-//       body: Stack(
-//         fit: StackFit.expand,
-//         children: [
-//           // buildMap(),
-//           // buildBottomNavigationBar(),
-//           buildFloatingSearchBar(),
-//         ],
-//       ),
-//     );
-//   }
-
-//   Widget buildFloatingSearchBar() {
-//     // final isPortrait =
-//     //     MediaQuery.of(context).orientation == Orientation.portrait;
-
-//     return FloatingSearchBar(
-//       hint: 'Search...',
-//       scrollPadding: const EdgeInsets.only(top: 16, bottom: 56),
-//       transitionDuration: const Duration(milliseconds: 800),
-//       transitionCurve: Curves.easeInOut,
-//       physics: const BouncingScrollPhysics(),
-//       // axisAlignment: isPortrait ? 0.0 : -1.0,
-//       openAxisAlignment: 0.0,
-//       // width: isPortrait ? 600 : 500,
-//       debounceDelay: const Duration(milliseconds: 500),
-//       onQueryChanged: (query) {
-//         // Call your model, bloc, controller here.
-//       },
-//       // Specify a custom transition to be used for
-//       // animating between opened and closed stated.
-//       transition: CircularFloatingSearchBarTransition(),
-//       actions: [
-//         FloatingSearchBarAction(
-//           showIfOpened: false,
-//           child: CircularButton(
-//             icon: const Icon(Icons.place),
-//             onPressed: () {},
-//           ),
-//         ),
-//         FloatingSearchBarAction.searchToClear(
-//           showIfClosed: false,
-//         ),
-//       ],
-//       builder: (context, transition) {
-//         return ClipRRect(
-//           borderRadius: BorderRadius.circular(8),
-//           child: Material(
-//             color: Colors.white,
-//             elevation: 4.0,
-//             child: Column(
-//               mainAxisSize: MainAxisSize.min,
-//               children: Colors.accents.map((color) {
-//                 return Container(height: 112, color: color);
-//               }).toList(),
-//             ),
-//           ),
-//         );
-//       },
-//     );
-//   }
-// }
-
 class _Tours extends State<Tours> {
   Widget build(BuildContext context) {
     return Scaffold(
+      // backgroundColor: Colors.white70,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(160.0),
         child: Stack(
@@ -121,46 +54,14 @@ class _Tours extends State<Tours> {
   }
 
   Widget buildFloatingSearchBar() {
-    // final isPortrait =
-    //     MediaQuery.of(context).orientation == Orientation.portrait;
-
-    return FloatingSearchBar(
-      // automaticallyImplyDrawerHamburger: false,
-      automaticallyImplyBackButton: false,
-      hint: 'Search...',
-      onQueryChanged: (query) {
-        // Call your model, bloc, controller here.
-      },
-      // Specify a custom transition to be used for
-      // animating between opened and closed stated.
-      // transition: CircularFloatingSearchBarTransition(),
-      actions: [
-        FloatingSearchBarAction(
-          showIfOpened: false,
-          child: CircularButton(
-            icon: const Icon(Icons.place),
-            onPressed: () {},
-          ),
-        ),
-        // FloatingSearchBarAction.searchToClear(
-        //   showIfClosed: false,
-        // ),
-      ],
-      builder: (context, transition) {
-        return ClipRRect(
-          // borderRadius: BorderRadius.circular(8),
-          child: Material(
-            color: Colors.white,
-            // elevation: 4.0,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              // children: Colors.accents.map((color) {
-              //   return Container(height: 112, color: color);
-              // }).toList(),
-            ),
-          ),
-        );
-      },
-    );
+    return TextFormField(
+        style:
+            new TextStyle(fontWeight: FontWeight.normal, color: Colors.white),
+        decoration: InputDecoration(
+          hintText: 'Email',
+          contentPadding:
+              new EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(70.0)),
+        ));
   }
 }
