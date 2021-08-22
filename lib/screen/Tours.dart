56082d96aa4b4a4c572cb767cfdd5c7a1845c965
sern <sanghit@hotmail.com>
2021-08-22 12:13:16 +0700

    add icon right side search bar

--- lib/screen/Tours.dart
+++ lib/screen/Tours.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'TestLinkScreen.dart';
+
 class Tours extends StatefulWidget {
   @override
   _Tours createState() => new _Tours();
@@ -8,52 +10,83 @@ class Tours extends StatefulWidget {
 class _Tours extends State<Tours> {
   Widget build(BuildContext context) {
     return Scaffold(
-      // backgroundColor: Colors.white70,
+      backgroundColor: const Color(0xFFF6F6F6),
       appBar: PreferredSize(
-        preferredSize: Size.fromHeight(160.0),
+        preferredSize: Size.fromHeight(200.0),
         child: Stack(
           children: [
             Container(
               child: Padding(
                 padding: const EdgeInsets.only(top: 40, left: 15),
-                child: Row(
+                child: Column(
                   children: [
-                    Icon(
-                      Icons.menu,
-                      size: 30,
-                      color: Colors.black,
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.menu,
+                          size: 30,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          'เลือกแพทย์ที่คุณต้องการ',
+                          style: TextStyle(
+                            fontFamily: 'FC Minimal',
+                            fontSize: 30,
+                            color: const Color(0xff111111),
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Text(
-                      'เลือกแพทย์ที่คุณต้องการ',
-                      style: TextStyle(
-                        fontFamily: 'FC Minimal',
-                        fontSize: 30,
-                        color: const Color(0xff111111),
-                      ),
-                    ),
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 3,
+                          child: buildTextFormField(),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    //navigator to login with facebook
+                                    builder: (context) => TestLinkScreen()));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: Image.asset(
+                              'assets/images/levels.png',
+                              width: 23,
+                              height: 23,
+                            ),
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    //navigator to login with facebook
+                                    builder: (context) => TestLinkScreen()));
+                          },
+                          child: Padding(
+                            padding: const EdgeInsets.only(bottom: 20),
+                            child: Image.asset(
+                              'assets/images/place.png',
+                              color: const Color(0xff727272),
+                              width: 23,
+                              height: 23,
+                            ),
+                          ),
+                        ),
+                      ],
+                    )
                   ],
                 ),
               ),
             ),
-            Row(
-              children: [
-                buildTextFormField(),
-                Icon(Icons.menu),
-              ],
-            )
-            // buildTextFormField(),
-            // Column(
-            //   crossAxisAlignment: CrossAxisAlignment.end,
-            //   // verticalDirection: VerticalDirection.up,
-            //   children: [
-            //     buildTextFormField(),
-            //     Icon(Icons.ac_unit),
-            //     Icon(Icons.access_alarm),
-            //   ],
-            // ),
           ],
         ),
       ),
@@ -66,20 +99,31 @@ class _Tours extends State<Tours> {
   }
 
   Widget buildTextFormField() {
-    return Container(
-      height: 125,
-      child: Padding(
-        padding: const EdgeInsets.only(top: 90, left: 20, right: 70),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 15),
+      child: Container(
+        // height: 125,
+        decoration: BoxDecoration(
+          color: const Color(0xFFFFFFFF),
+          borderRadius: new BorderRadius.all(
+            new Radius.circular(20),
+          ),
+          // border: Border.all(color: Colors.black),
+        ),
         child: TextFormField(
             style:
                 TextStyle(fontWeight: FontWeight.normal, color: Colors.white),
             decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(14),
               hintText: 'Search here',
               prefixIcon: Icon(Icons.search),
-              contentPadding:
-                  new EdgeInsets.symmetric(vertical: 1.0, horizontal: 20.0),
-              border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(100.0)),
+              border: InputBorder.none,
+              // enabledBorder: InputBorder.none,
+              // focusedBorder: InputBorder.none,
+              // contentPadding:
+              //     new EdgeInsets.symmetric(vertical: 1.0, horizontal: 20.0),
+              // border:
+              //     OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
             )),
       ),
     );
