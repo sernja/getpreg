fcb18bf3c611d9b33443b5645d73c143335a27f7
sern <sanghit@hotmail.com>
2021-08-25 11:00:33 +0700

    add icon profile like and border bottom line in ListTours

--- lib/widgets/ListTours.dart
+++ lib/widgets/ListTours.dart
@@ -19,14 +19,13 @@ class ListTours extends StatelessWidget {
       leading: CircleAvatar(
         backgroundImage: AssetImage('assets/images/img_3.png'),
       ),
-      title: Text('นพ.a'),
-      subtitle: Text('Private clinic'),
-      // title: CustomListItem(
-      //   profileImg: 'assets/images/img_3.png',
-      //   user: 'Private clinic',
-      //   place: 'แพทย์ศาสตร์ จุฬา',
-      //   title: 'นพ.a',
-      // ),
+      // title: Text('นพ.a'),
+      title: CustomListItem(
+        title: 'นพ.a',
+        place: 'Private clinic',
+        university: 'แพทย์ศาสตร์ จุฬา',
+      ),
+      shape: Border(bottom: BorderSide(color: Colors.black.withOpacity(0.5), width: 2)),
     );
   }
 }
@@ -36,16 +35,14 @@ class CustomListItem extends StatelessWidget {
     Key key,
     // this.thumbnail,
     this.title,
-    this.user,
     this.place,
-    this.profileImg,
+    this.university,
   }) : super(key: key);
 
   // final Widget thumbnail;
   final String title;
-  final String user;
   final String place;
-  final String profileImg;
+  final String university;
 
   @override
   Widget build(BuildContext context) {
@@ -62,13 +59,23 @@ class CustomListItem extends StatelessWidget {
             flex: 3,
             child: _VideoDescription(
               title: title,
-              user: user,
               place: place,
+              university: university,
             ),
           ),
-          const Icon(
-            Icons.more_vert,
-            size: 16.0,
+          Image.asset('assets/images/star.png'),
+          Text(
+            '5.0',
+            style: TextStyle(
+              fontFamily: 'Lato',
+              fontSize: 30,
+              color: const Color(0xff111111),
+              fontWeight: FontWeight.w700,
+              height: 1.0666666666666667,
+            ),
+            textHeightBehavior:
+                TextHeightBehavior(applyHeightToFirstAscent: false),
+            textAlign: TextAlign.left,
           ),
         ],
       ),
@@ -80,13 +87,13 @@ class _VideoDescription extends StatelessWidget {
   const _VideoDescription({
     Key key,
     this.title,
-    this.user,
     this.place,
+    this.university,
   }) : super(key: key);
 
   final String title;
-  final String user;
   final String place;
+  final String university;
 
   @override
   Widget build(BuildContext context) {
@@ -98,19 +105,82 @@ class _VideoDescription extends StatelessWidget {
           Text(
             title,
             style: const TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 14.0,
+              fontFamily: 'Lato',
+              fontSize: 15,
+              color: const Color(0xff111111),
+              fontWeight: FontWeight.w600,
+              height: 2.1333333333333333,
             ),
+            textHeightBehavior:
+                TextHeightBehavior(applyHeightToFirstAscent: false),
           ),
           const Padding(padding: EdgeInsets.symmetric(vertical: 2.0)),
-          Text(
-            user,
-            style: const TextStyle(fontSize: 10.0),
+          Row(
+            children: [
+              Image.asset(
+                'assets/images/placeholder2.png',
+                color: Color(0xFF1111111).withOpacity(0.5),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                place,
+                style: const TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 13,
+                  color: const Color(0x80111111),
+                  height: 1.2307692307692308,
+                ),
+                textHeightBehavior:
+                    TextHeightBehavior(applyHeightToFirstAscent: false),
+              ),
+            ],
           ),
           const Padding(padding: EdgeInsets.symmetric(vertical: 1.0)),
-          Text(
-            place,
-            style: const TextStyle(fontSize: 10.0),
+          Row(
+            children: [
+              Image.asset(
+                'assets/images/graduate-cap.png',
+                color: Color(0xFF1111111).withOpacity(0.5),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                university,
+                style: const TextStyle(
+                  fontFamily: 'Lato',
+                  fontSize: 13,
+                  color: const Color(0x80111111),
+                  height: 1.2307692307692308,
+                ),
+                textHeightBehavior:
+                    TextHeightBehavior(applyHeightToFirstAscent: false),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0),
+            child: Stack(
+              // fit: StackFit.passthrough,
+              clipBehavior: Clip.none,
+              children: [
+                Image.asset('assets/images/img.png'),
+                Positioned(
+                  left: 10,
+                  child: Image.asset('assets/images/img_1.png'),
+                ),
+                Positioned(
+                  left: 20,
+                  child: Image.asset('assets/images/img_2.png'),
+                ),
+                Positioned(
+                  left: 30,
+                  child: Image.asset('assets/images/like.png'),
+                )
+              ],
+            ),
           ),
         ],
       ),
